9d66b8896e35565a9affd90c55503c7f172feb4c
jyj97 <122950904+jyj97@users.noreply.github.com>
2024-10-03 16:42:03 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+
+li = list(range(1, n + 1))
+
+com_li = [0] * m
+
+def combi(cnt, idx):
+    if cnt == m:
+        print(" ".join(map(str, com_li)))
+        return
+    
+    for i in range(idx, n):
+        com_li[cnt] = li[i]
+        combi(cnt + 1, i + 1)
+        
+
+
+combi(0,0)
