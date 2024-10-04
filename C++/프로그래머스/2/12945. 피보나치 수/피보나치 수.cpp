68ef1c03c54fd32c33bb9dc2825e9180493ce6af
jyj97 <122950904+jyj97@users.noreply.github.com>
2024-10-04 22:38:04 +0900

    [level 2] Title: 피보나치 수, Time: 0.53 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int *fibo = new int[n + 1];
+    fibo[0] = 0; fibo[1] = 1;
+    for (int i = 2; i <= n; i++){
+        fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567; 
+    }
+    
+    answer = fibo[n];
+    delete[] fibo;
+    return answer;
+}
